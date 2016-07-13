7bee9846d3251a8f9deb19f33ec41b56f574fa56
Dane Cravens <cdcravens@moreheadstate.edu>
2016-07-13 16:39:54 -0400

    Update main.js
    Eliminated useless variable keyClickCounter. Added functionality for changing pen color.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-keyClickCounter = 1;
-gridSize = 80;
+gridSize = 80;      //Stores the length and width value of the game board. default 80.
+penColor = 'blue';  //Stores the color value of the pen. default blue.
 
+
+//Draws the initial game board with a length and width of 80 cells.
 $(document).ready(function() {
     var $grid = $('.grid');
     
@@ -16,6 +18,7 @@ $(document).ready(function() {
     } 
 });
 
+//Creates a square 'board' of div's. length and width are defined by the gridSize //variable.
 function createBoard(gridSize) {
     var $grid = $('.grid');
     
@@ -34,26 +37,61 @@ function createBoard(gridSize) {
     $('.row-cells').css('width', Math.ceil(800/gridSize));
 }
 
+
+//Erases all div's associated with the .row-cells class
 function eraseBoard() {
     $('.row-cells').remove();
 }
 
+
+//One left click begins the draw effect.
 $('.grid').on('click', function() {
     $('.row-cells').hover(function() {
-        $(this).css('background-color', 'blue');
+        $(this).css('background-color', penColor);
         
+        //double-clicking the left mouse button ends the draw effect.
         $('.grid').on('dblclick', function() {
             $('.row-cells').unbind('mouseenter mouseleave');
         });
     });
 });
 
+
+//Changes the background color of all grid cells to the default white.
 $('#clear').on('click', function() {
     $('.row-cells').css('background-color', 'white');
 });
 
+
+//Erases the board and creates a new one.
 $('#generate').on('click', function() {
     eraseBoard();
     gridSize = prompt("Please choose the size of the sketchpad (2 - 128)");
     createBoard(gridSize);
+});
+
+
+//Change the pen color by clicking on the respective color in the drop-down menu.
+$('#color-red').on('click', function() {
+    penColor = 'red';
+});
+
+$('#color-blue').on('click', function() {
+    penColor = 'blue';
+});
+
+$('#color-purple').on('click', function() {
+    penColor = 'purple';
+});
+
+$('#color-yellow').on('click', function() {
+    penColor = 'yellow';
+});
+
+$('#color-green').on('click', function() {
+   penColor = 'green'; 
+});
+
+$('#color-orange').on('click', function() {
+    penColor = 'orange';
 });
